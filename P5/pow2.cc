98f3bae7ec5edf2ef140223eeae489bde184e504
ssm116-ua <ssm116@alu.ua.es>
2025-03-13 14:18:44 +0100

    Se ha modificado la práctica

--- P5/pow2.cc
+++ P5/pow2.cc
@@ -1,8 +1,23 @@
+// 51253936T Salvador Pardo Saiz
 #include <iostream>
+#include <iomanip>
+#include <math.h>
 
 using namespace std;
 
-// 
+void printTable()
+{
+        cout << "# Comparison Pow(2, n)."
+        << endl
+        << "# Average processing Msteps (millions of program steps)"
+        << endl
+        << "#" << " Size" <<setw(15) << "Pow2_1(n)" <<setw(15) <<"Pow2_2(n)"<<setw(15) <<"Pow2_3(n)" 
+        << endl
+        << "#===================================================================================="<< endl;
+}
+
+
+// coste: \ Theta (n) 
 unsigned long pow2_1(unsigned n)
 {
     if( n == 0)
@@ -15,6 +30,7 @@ unsigned long pow2_1(unsigned n)
     }
 }
 
+// coste: \ Theta (n²) 
 unsigned long pow2_2(unsigned n)
 {
     unsigned long result = 1;
@@ -25,11 +41,11 @@ unsigned long pow2_2(unsigned n)
     else
     {
         
-        for (int i = 0; i < n; i++) {  // Controla la potencia (multiplicaciones)
-            int temp = 0;
+        for (unsigned i = 0; i < n; i++) {  // Controla la potencia (multiplicaciones)
+            unsigned long temp = 0;
             
             // Simula la multiplicación por 2 usando sumas
-            for (int j = 0; j < result; j++) {
+            for (unsigned long j = 0; j < result; j++) {
                 temp += 2;
             }
 
@@ -39,13 +55,38 @@ unsigned long pow2_2(unsigned n)
     return result;
 }
 
-//unsigned long pow2_3(unsigned n)
-//{
-    
-//}
+// coste: \ Theta (log n) 
+unsigned long pow2_3(unsigned n)
+{
+    unsigned long operation;
+    if(n == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        operation = pow2_3(n/2);
+
+        if(n%2 == 0) // Si es par
+        {
+            return operation*operation;
+        }
+        else
+        {
+            return operation*operation*2;
+        }
+
+    }
+}
 
 int main()
 {
-    cout<< pow2_1(3) << endl
-    << pow2_2(3) << endl;
+    if(pow2_1(4) == pow(2,4) && pow2_2(8) && pow(2,8) && pow2_3(16) == pow(2,16))
+    {
+        printTable();
+    }
+    else
+    {
+        cout << "Falla algún módulo" <<endl;
+    }
 }
