08c4460f07eb9f421aeea784688479ce3372913c
ssm116-ua <ssm116@alu.ua.es>
2025-03-14 19:55:28 +0100

    Se ha completado la práctica 5

--- P5/pow2.cc
+++ P5/pow2.cc
@@ -5,33 +5,46 @@
 
 using namespace std;
 
+#define tamVec 15
+
 void printTable()
 {
         cout << "# Comparison Pow(2, n)."
         << endl
-        << "# Average processing Msteps (millions of program steps)"
+        << "# Average processing steps"
         << endl
-        << "#" << " Size" <<setw(15) << "Pow2_1(n)" <<setw(15) <<"Pow2_2(n)"<<setw(15) <<"Pow2_3(n)" 
+        << "#" << " n Values" <<setw(20) << "Pow2_1" <<setw(20) <<"Pow2_2"<<setw(20) <<"Pow2_3" 
         << endl
-        << "#===================================================================================="<< endl;
+        << "#================================================================================="<< endl;
 }
 
+void fillVector(unsigned int v[])
+{
+    unsigned int n = 6;
+    for (unsigned int i = 0; i < tamVec; i++)
+    {
+        v[i] = n;
+        n++;
+    }
+    return;
+}
 
 // coste: \ Theta (n) 
-unsigned long pow2_1(unsigned n)
+unsigned long pow2_1(unsigned n, unsigned long &steps)
 {
+    steps++;
     if( n == 0)
     {
         return 1;
     }
     else
     {
-        return 2 * pow2_1(n-1);
+        return 2 * pow2_1(n-1, steps);
     }
 }
 
 // coste: \ Theta (n²) 
-unsigned long pow2_2(unsigned n)
+unsigned long pow2_2(unsigned n, unsigned long &steps)
 {
     unsigned long result = 1;
     if(n == 0)
@@ -43,10 +56,12 @@ unsigned long pow2_2(unsigned n)
         
         for (unsigned i = 0; i < n; i++) {  // Controla la potencia (multiplicaciones)
             unsigned long temp = 0;
-            
+            steps++;
             // Simula la multiplicación por 2 usando sumas
-            for (unsigned long j = 0; j < result; j++) {
+            for (unsigned long j = 0; j < result; j++) 
+            {
                 temp += 2;
+                steps++;
             }
 
             result = temp;  // Actualizamos el resultado
@@ -56,17 +71,19 @@ unsigned long pow2_2(unsigned n)
 }
 
 // coste: \ Theta (log n) 
-unsigned long pow2_3(unsigned n)
+unsigned long pow2_3(unsigned n, unsigned long &steps)
 {
     unsigned long operation;
+    steps++;
+
     if(n == 0)
     {
         return 1;
     }
     else
     {
-        operation = pow2_3(n/2);
-
+        operation = pow2_3(n/2, steps);
+        
         if(n%2 == 0) // Si es par
         {
             return operation*operation;
@@ -81,12 +98,26 @@ unsigned long pow2_3(unsigned n)
 
 int main()
 {
-    if(pow2_1(4) == pow(2,4) && pow2_2(8) && pow(2,8) && pow2_3(16) == pow(2,16))
+    unsigned int size[tamVec];
+    unsigned long steps1 = 0, steps2 = 0, steps3 = 0;
+
+    fillVector(size);    
+
+    printTable();
+
+    for (unsigned int i = 0; i < tamVec; i++)
     {
-        printTable();
+        steps1 = 0;
+        steps2 = 0;
+        steps3 = 0;
+
+        if(pow2_1(size[i],steps1) != pow(2, size[i]) || pow2_2(size[i],steps2) != pow(2, size[i]) || pow2_3(size[i],steps3) != pow(2, size[i]))
+        {
+            cout << "Falla algún módulo" <<endl;
+            return 1;
+        }
+
+        cout << setw(5) << size[i]<< setw(20) << steps1 << setw(20) << steps2 << setw(20) << steps3 <<endl;
     }
-    else
-    {
-        cout << "Falla algún módulo" <<endl;
-    }
+    return 0;
 }
---
+++ P5/validate_Pr5.sh
@@ -0,0 +1,268 @@
+#!/bin/bash
+
+#Algoritmia (Ing. Robótica - DLSI-UA) 2024-25
+#Validación del archivo de la práctica 1 que se pretende entregar
+#       USAGE: $0 tar-gz-file [-h] [--debug] [--quiet] [-d dir_home]
+
+
+export PATH=.:$PATH
+
+#ATENCIÓN USUARIOS DE MAC:
+#puede que no tengas la orden 'timeout' en el sistema (lo puedes saber escribiendo 'timeout' en la terminal);
+#en tal caso, deja la variable "TIMEOUT" vacía (a continuación de este comentario)
+#pero ten en cuenta que ya NO se detectará si tu código se cuelga o si produce una violación de segmento
+#tendrás que asegurarte tú mism@ de que eso no ocurre (al igual que si compilas con un MAC)
+
+#TIMEOUT=""  #para el caso de no disponer de la orden 'timeout' (pero ten en cuenta que en la corrección sí se usará timeout)
+TIMEOUT="timeout"
+
+# variables to be assigned in each job
+num_practica="5"
+source="pow2.cc"
+exe="pow2"
+err_file="errors_pr5.log"
+warn_file="warnings_pr5.log"
+in_tgz_must_be_files="${source} makefile pow2.gpi"
+allowed_files="${in_tgz_must_be_files} ${err_file} ${warn_file}"
+make_must_build="${exe} pow2.png"
+MIN_TIME=0
+MAX_TIME=7
+
+#Control variables
+[ ! -z ${TIMEOUT} ] && { TIMEOUT="${TIMEOUT} ${MAX_TIME}"; }
+RETURN_CODE=0
+allowed_ext=".tar.gz .tgz"
+warnings=0
+name="<unknown>"
+captured=""
+msg_timeout=""
+check_num=0
+
+filename=""     #file (tgz) to validate
+dir_home=$(pwd) #directory where is TEST subdirectory. Assumed current directory
+dir_tgz=""      #directory where tgz is
+quiet=0
+debug=0
+
+#argument management
+args=( "$@" )
+for ((i=0; i<${#args[@]}; i++)); do
+    is_filename=1
+    [[ ${args[$i]} == "--debug" ]] && { debug=1; is_filename=0; }
+    [[ ${args[$i]} == "--quiet" ]] && { quiet=1; is_filename=0; }
+    [[ ${args[$i]} == "-d" ]] && { i=$((i+1)); dir_home=${args[$i]}; is_filename=0; }
+    [[ ${args[$i]} == "-h" ]] && { echo "help: $0 tar.gz-file [-h] [--debug] [--quiet] [-d dir_home]";  exit 255; }
+    [[ is_filename -eq 1 ]] && filename="${args[$i]}"
+done
+
+[[ $quiet -eq 0 ]] && echo "Entrega de la práctica ${num_practica}"
+[[ $quiet -eq 0 ]] && echo "Fichero a validar: ${filename}"
+
+[   -z "$filename" ] && { echo $0: missing tar.gz file; exit 255; }
+[ ! -f "$filename" ] && { echo $0: file \'$filename\' not found; exit 255; }
+
+
+
+#Create tmp directory
+tmp_dir=$(mktemp -d /tmp/validate_dir.XXXXXX)
+
+# Actions at the end
+by_the_end() {
+    exit_code=$?
+    [[ -f ${err_file} ]] && exit_code=1
+    [[ -f ${warn_file} ]] && warnings=1
+    [[ $exit_code -ne 0 ]] && RETURN_CODE=$((RETURN_CODE |= 1))
+    if [[ $quiet -eq 0 ]]; then
+        [[ -f ${warn_file} ]] && { echo; cat ${warn_file}; }
+        [[ $warnings  -ne 0 ]] && echo -e "\nATENCIÓN: Los avisos mostrados pueden mermar considerablemente la nota."
+        [[ -f ${err_file} ]] && { echo; cat ${err_file}; }
+        if [[ $exit_code -eq 0 ]]; then
+            echo -e "\nDNI/NIE asociado al archivo comprimido: "$dni
+            echo -e "Nombre capturado en la primera línea de ${source}: "$name
+            echo -e "Asegúrate de que la captura mostrada  de DNI/NIE y nombre es correcta."
+            echo -e "\nVALIDACIÓN POSITIVA: El trabajo cumple las especificaciones básicas (no se comprueban todas), pero asegúrate de que también compila en los ordenadores del aula."
+        else
+            echo -e "\nVALIDACIÓN NEGATIVA: Debes corregir los errores antes de entregar."
+        fi
+#    else
+#        echo -n $RETURN_CODE
+    fi
+    rm -r $tmp_dir
+    exit $RETURN_CODE
+}
+
+
+check_err_args() {
+    error_message=$($1 2>&1 1>/dev/null)
+    ret=$? # 124=timeout; 134=seg-fault; 139=core-dumped
+    if [[ ret -eq 124 ]] || [[ ret -eq 134 ]] || [[ ret -eq 139 ]]; then
+        echo "Aviso: '$2' produce violación de segmento o 'timeout'" >> ${warn_file}
+        warnings=1
+    fi
+    if [[ -z "$error_message" ]]; then
+        echo "Aviso: '$2' no emite mensaje de error (por la salida de error)"  >> ${warn_file}
+        warnings=1
+    fi 
+    [[ warnings -eq 1 ]] &&  RETURN_CODE=$(( RETURN_CODE |= 2 ))
+}
+
+check_code() {
+    gprof -p -b $1 gmon.out | grep $2
+    if [ $? -ne 0 ]; then
+        check_call_funct $2
+    fi
+    check_memory_map --save $1 $2    
+}
+
+error_msg(){
+    echo -e "\n*** Validación número $3 ***" >> ${err_file}
+    echo -e "  Prueba realizada: $2" >> ${err_file}
+    echo -e "  $1" >> ${err_file}
+    RETURN_CODE=$(( RETURN_CODE |= 1 ))
+}
+
+launch() {
+    [[ -f tmp_file ]] && rm tmp_file
+    captured=$($1 2>tmp_file); ret=$?
+    case $ret in
+	    124) msg_timeout="tarda demasiado ('timeout').";;
+	    134) msg_timeout="produce violación de segmento.";;
+	    139) msg_timeout="produce un 'core'.";;
+	    *)   [[ -s tmp_file ]] && { msg_timeout="no debe mostrar nada por la salida de error puesto que es correcta."; ret=1; } || ret=0
+    esac
+    [[ $ret -ne 0 ]] && error_msg "Error: la orden ${msg_timeout}" "$2" $3
+    return $ret
+}
+
+function validate_n_nums() {
+# códigos de error: 
+#   1:más elementos de lo especificado  $2; 
+#   2:alguno de los elementos no son números
+captured_array=()
+read -a captured_array < <(echo $1)
+[[ ${#captured_array[@]} -ne $2 ]] && return 1
+d=[0-9]
+for ((i=0; i<${#captured_array[@]}; i++)); do
+    [[ ! ${captured_array[i]} =~ ^$d+([.]$d+)?([Ee][-+]?$d+)?$ ]] && return 2
+done
+return 0
+}
+
+
+#capture signals
+trap by_the_end EXIT SIGINT SIGTERM
+
+
+# Work in tmp dir; capture tgz name and directory where it is
+cp "$filename" $tmp_dir
+cd "$(dirname "${filename}")"
+dir_tgz=$(pwd)
+filename="$(basename "${filename}")" #filename=${filename##*/}
+cd $tmp_dir
+
+
+
+# dni/nie & extension of the file to be delivered
+check_num=1
+dni=$(echo "$filename" | cut -d "." -f 1)
+ext=.$(echo "$filename" | cut -d "." -f 2-3) # | tr -d '\n' | tail -c 2)
+if [[ ! " $allowed_ext " =~ " $ext " ]];then
+    echo "Error: Las extensiones permitidas son: {"$allowed_ext"}."  >> ${err_file}
+    echo -e "\t(extensión capturada: $ext )" >> ${err_file}
+    RETURN_CODE=$((RETURN_CODE |= 1))
+    exit 1
+fi
+
+# unpack tgz
+check_num=2
+tar -xzvf "$filename" > /dev/null
+if [ $? -ne 0 ]; then
+    echo "Error: 'tar' falló al desempaquetar ${filename}." >> ${err_file}
+    RETURN_CODE=$((RETURN_CODE |= 1))
+    exit 1
+fi
+
+# keep only what is stored in the tgz
+rm "$filename"
+[[ -f ${err_file}  ]] && rm ${err_file}
+[[ -f ${warn_file} ]] && rm ${warn_file}
+
+
+# Verify that has been delivered what is requested
+check_num=3
+missing=""
+for f in ${in_tgz_must_be_files}; do
+    if [ ! -f $f ]; then
+        missing=${missing}" "${f}
+    fi 
+done
+if [[ ! -z ${missing} ]]; then
+    echo "Error: Falta/n archivo/s en la entrega (${missing} )." >> ${err_file}
+    RETURN_CODE=$((RETURN_CODE |= 1))
+fi
+
+
+# verify that only what is necessary is delivered
+check_num=4
+unwanted=""
+[[ -f Makefile ]] && mv Makefile makefile
+for f in *; do
+    if [[ ! " $allowed_files " =~ " $f " ]];then 
+        unwanted=${unwanted}" "${f}
+    fi
+done
+if [[ ! -z ${unwanted} ]]; then
+    echo "Error: Se entregan más archivos de los permitidos (${unwanted} )." >> ${err_file}
+    RETURN_CODE=$((RETURN_CODE |= 1))
+fi
+
+
+#name="$(echo -e "${name}" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')" # All-TRIM
+#tr -d "[:space:]" #elimina tb saltos de linea, a diferencia de tr -d "" que elimina solo espacios
+# Verify the student's name in the first line of the source (only first line is checked)
+check_num=5
+[[ ! -f ${source} ]] && { RETURN_CODE=$((RETURN_CODE |= 1)); exit 1; }
+name=$(cat ${source} | head -n 1 | tr '*' '/' | xargs)
+[[ "$name" =~ ^"//" ]] && name=$(echo ${name} | tr -d '/' | xargs) || name="<unknown>"
+[[ -z ${name} ]] && name="<unknown>"
+if [[ "$name" == "<unknown>" ]]; then
+    echo "Error: No se captura tu nombre en la primera línea del archivo ${source}." >> ${err_file}
+    RETURN_CODE=$((RETURN_CODE |= 1))
+fi
+
+#compilation
+check_num=6
+start=$(date +%s)
+$TIMEOUT make &> /dev/null
+ret=$?
+end=$(date +%s)
+elapsed_t=$(echo $(($end - $start)) )
+[ $quiet -eq 0 ] && echo -n "Tiempo de proceso (seg.): "; [ $ret -eq 124 ]  && echo "TIMEOUT(${MAX_TIME})" || echo ${elapsed_t}
+case $ret in
+    124) msg_timeout="'make' se lleva un tiempo excesivo (> ${MAX_TIME} seg.)";;
+    134) msg_timeout="El programa produce una violación de segmento";;
+    139) msg_timeout="El programa produce una violación de segmento";;
+    *)   [[ ret -ne 0 ]] && { msg_timeout="'make' ha fallado ($ret)"; };;
+esac
+[[ ret -ne 0 ]] && { echo Error: $msg_timeout >> ${err_file}; RETURN_CODE=$((RETURN_CODE |= 1)); exit 1; }
+
+# verify that 'make' builds all files
+check_num=7
+are_not=""
+for f in ${make_must_build}; do
+    if [ ! -f $f ]; then
+         are_not=${are_not}" "${f}
+    fi 
+done
+if [[ ! -z ${are_not} ]]; then
+    echo "Error: 'make' no crea todo los archivos que se piden (${are_not})." >> ${err_file}
+    RETURN_CODE=$((RETURN_CODE |= 1))
+fi
+
+
+
+( [[ -f ${err_file} ]] || [[ $((RETURN_CODE&1)) -eq 1 ]] ) && exit 1;
+
+exit 0
+
+
